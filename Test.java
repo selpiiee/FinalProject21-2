7304d85c21d65774fd0bb913d246972de2287c2d
clevymargareth <clevybria236@gmail.com>
2025-10-02 15:09:47 +0700

    user 3

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test{
     public static void main(String [] args) {
-        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 3");
+        System.out.println("User 3 menambahkan baris 3");
+        System.out.println("clevy imoet");
     }
 }
