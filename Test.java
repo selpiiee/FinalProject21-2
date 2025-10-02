a888315d274dbff419a073af87fc273fded67484
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-10-02 15:03:47 +0700

    User 1

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
     public static void main(String [] args) {
-        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Hi, ini baris pertama yang dimodivikasi User 1");
+        System.out.println("User 1 menambahkan baris baru");
     }
 }
