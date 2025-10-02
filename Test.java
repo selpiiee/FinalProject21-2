cc336bcbf976f9ed73cd99cb1becd11afc2e6a7e
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-10-02 15:08:56 +0700

    user 1 - edit

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
     public static void main(String [] args) {
-        System.out.println("Hi, ini baris pertama yang dimodivikasi User 1");
-        System.out.println("User 1 menambahkan baris baru");
+        System.out.println("Hi, this is the first line modified by User 1.");
+        System.out.println("User 1 added line");
         System.out.println("User 2 added line");
     }
 }
